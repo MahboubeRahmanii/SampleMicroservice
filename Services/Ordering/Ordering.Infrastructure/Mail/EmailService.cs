95407cf2efd4e2386e66dd727a6e8a9eac021f27
mahboube <rahmanim93@yahoo.com>
2024-05-30 15:59:20 +0330

    first commit

---
+++ Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Mail
+{
+    internal class EmailService : IEmailService
+    {
+        public async Task<bool> SendEmail(Email email)
+        {
+            return true;
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=OrderDb;Integrated Security=true");
+
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+}
